38e7994466645dad7fb15ec9cf6c1891ac096c77
Loz <tuanchu24112004@gmail.com>
2024-10-27 16:34:00 +0700

    Xoa khuyen mai

--- Models/ChiTietHdn.cs
+++ Models/ChiTietHdn.cs
@@ -13,8 +13,6 @@ public partial class ChiTietHdn
 
     public int? Slnhap { get; set; }
 
-    public string? KhuyenMai { get; set; }
-
     public virtual HoaDonNhap MaHdnNavigation { get; set; } = null!;
 
     public virtual SanPham MaSpNavigation { get; set; } = null!;
