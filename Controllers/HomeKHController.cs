302eb07a003b159069ba939746aefc3e07677db4
Tuan <tuanchu24112004@gmail.com>
2024-11-01 09:41:26 +0700

    C

--- Controllers/HomeKHController.cs
+++ Controllers/HomeKHController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BTL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BTL.Controllers
 {
     public class HomeKHController : Controller
     {
+        private readonly QlhieuThuocContext _db;
+
+        public HomeKHController(QlhieuThuocContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var products = _db.SanPhams.Include(c => c.MaLtNavigation).ToList();
+            return View(products);
         }
     }
 }
